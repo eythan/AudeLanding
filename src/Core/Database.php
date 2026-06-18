1b59f0699fc9c48a23776a0a29c51e6b6a8c5844
Eythan Franceus <eythan.franceus@gmail.com>
2026-06-18 15:18:12 +0200

    Fix

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -29,7 +29,7 @@ class Database
                     ]
                 );
             } catch (PDOException $e) {
-                die("Erreur de connexion à la base de données.");
+                die("Erreur de connexion à la base de données : " . $e->getMessage());
             }
         }
         return self::$instance;
